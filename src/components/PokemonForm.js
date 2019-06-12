254ebcd3cb3377296b53d618e09f9e23e99b3e48
shawn0527 <shawnren527@gmail.com>
2019-06-11 21:43:18 -0500

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,6 +13,33 @@ class PokemonForm extends React.Component {
     }
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+    fetch('http://localhost:3000/pokemon', {
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+        'name': this.state.name,
+        'stats': [{
+          'value': this.state.hp,
+          'name': 'hp'
+        }],
+        'sprites': {
+          'front': this.state.frontUrl,
+          'back': this.state.backUrl
+        }
+      })
+    })
+    .then(res => res.json())
+    .then(pokemon => this.props.createPokemon(pokemon))
+    e.target.reset()
+  }
+
   render() {
     return (
       <div>
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,54 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      pokemons: [],
+      displayPokemons: [],
+      serachTerm: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then(data =>{
+      this.setState({
+        pokemons: data,
+        displayPokemons: data
+      })
+    })    
+  }
+
+  searchPokemon = (e, {value}) => {
+    let arr = this.state.pokemons.filter(pokemon => pokemon.name.includes(value))
+    this.setState({
+      displayPokemons: arr,
+      searchTerm: value
+    })
+  }
+
+  createPokemon = pokemon => {
+    let newArr = this.state.pokemons
+    newArr.push(pokemon) 
+    this.setState({
+      pokemons: newArr,
+      displayPokemons: newArr
+    })
+  }
+  
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={_.debounce((e, {value}) => this.searchPokemon(e, {value}), 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={this.state.displayPokemons}/>
         <br />
-        <PokemonForm />
+        <PokemonForm createPokemon={this.createPokemon}/>
       </div>
     )
   }
